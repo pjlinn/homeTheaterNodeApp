e503fd6fedeabbfdfbae1c263290e54889055521
pjlinn <p.linnehan@gmail.com>
2015-06-07 00:15:37 -0400

    Pretty sure this successfully mimics scholarly paper behavior.

--- fullHomeTheaterApp/client/js/controllers.js
+++ fullHomeTheaterApp/client/js/controllers.js
@@ -7,9 +7,17 @@ controllers.controller('MainCtrl', [
 	function($scope, $http, Televisions, Amplifiers, Speakers, Components) {
 		$scope.test = 'Hello World';
 
-		$scope.create = function() {
-			alert("working");
-		};
+		/* 
+			Function holder. It's logic is defined in a directive
+		 	I think I'm supposed to keep the ctrl fairly clean
+			I'm also not sure how to call a function defined in a
+			directive. I'd have to move the button inside the 
+			directive so that it would inherit the directives scope.
+			I don't want to do that, however. So I will bind the
+			directive to this function and define it there and have
+			the button call it.
+		*/
+		// $scope.visualizeIt = function() { console.log('controller...')};
 
 		/*
 			Different ways to use services and parameters. I can define
@@ -20,7 +28,24 @@ controllers.controller('MainCtrl', [
 		$scope.televisions = Televisions.query({component: 'television'});
 		$scope.amplifiers = Amplifiers.query();
 		$scope.speakers = Speakers.query();
-		$scope.components = Components.query();
+	
+		// Need to use promise for the direvtive watch
+		var componentResponse = Components.query();
+		componentResponse.$promise.then(function(result) {
+			$scope.components = result;
+		});
+
+		// For the options. Using objects is unnecessary in this case
+		$scope.axisValues = [
+			{ name:'Cost', value:'cost', index: 0},
+			{ name:'Performance', value:'performance', index: 1},
+			{ name:'Reliability', value:'reliability', index: 2}
+		];
+
+		$scope.xVariable = $scope.axisValues[0];
+		$scope.yVariable = $scope.axisValues[1];
+
+		$scope.systemDesigns = [];
 
 
 		// $http.get('/televisions/')
--- fullHomeTheaterApp/client/js/directives.js
+++ fullHomeTheaterApp/client/js/directives.js
@@ -16,23 +16,224 @@ directives.directive('ngWelcome', function() {
 });
 
 directives.directive('ngD3Chart', function() {
+	
 	var margin = {left: 75, right: 20, top: 30, bottom: 50},
 		height = 300 - margin.top - margin.bottom,
 		width = 1400 - margin.left - margin.right;
 
+	var tooltip = d3.select("#canvas")
+		.append("div")
+			.attr("class", "tooltip")
+			.style("opacity", 0);
+
+	var x = d3.scale.linear().range([0, width]);
+	var y = d3.scale.linear().range([height, 0]);
+
+	var xAxis = d3.svg.axis().scale(x)
+		.orient("bottom");
+	var yAxis = d3.svg.axis().scale(y)
+		.orient("left");
+
+	var svg = d3.select("#canvas")
+		.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top +")");
+
+	svg.append("g")
+			.attr("class", "xAxis")
+			.attr("transform", "translate(0, " + height + ")");
+
+	svg.append("g")
+			.attr("class", "yAxis");
+
+	svg.append("text")
+		.attr("class", "xLabel");
+
+	svg.append("text")
+		.attr("class", "yLabel");
+
 	return {
 		restrict: 'AE',
 		replace: false,
+		scope: {
+			optionX: '=',
+			optionY: '=',
+			systemDesigns: '='
+		},
 		link: function(scope, element, attrs) {
 
-			function renderGraph(televisions) {
-				function now() {
-					alert(scope.televisions);
+			function visualizeIt() {
+			
+				var xVariable = scope.optionX.value;
+				var yVariable = scope.optionY.value;
+
+				scope.systemDesigns.forEach(function(d) {
+					d.name = d.name;
+					d.xVariable = d[xVariable];
+					d.yVariable = d[yVariable];
+				});
+
+				x.domain([d3.min(scope.systemDesigns, function(d) { return d.xVariable; }) - 1, d3.max(scope.systemDesigns, function(d) { return d.xVariable; }) + 1]);
+				y.domain([d3.min(scope.systemDesigns, function(d) { return d.yVariable; }) - 1, d3.max(scope.systemDesigns, function(d) { return d.yVariable; }) + 1]);
+
+				var circle = svg.selectAll("circle")
+					.data(scope.systemDesigns);
+
+				circle.exit().remove();
+
+				circle.enter().append("circle")
+					.attr("r", 5)
+					.on("mouseover", function(d) {
+						tooltip.transition()
+							.duration(200)
+							.style("opacity", .9);
+						tooltip.html(d.name)
+							.style("left", (d3.event.pageX + 5) + "px")
+							.style("top", (d3.event.pageY - 28) + "px");
+					})
+					.on("mouseout", function(d) {
+						tooltip.transition()
+							.duration(500)
+							.style("opacity", 0);
+					});
+
+				circle
+					.attr("cx", function(d) { return x(d.xVariable); })
+					.attr("cy", function(d) { return y(d.yVariable); });
+
+				svg.select('.xAxis')
+					.call(xAxis);
+
+				svg.select('.yAxis')
+					.call(yAxis);
+
+				svg.select(".xLabel")
+					.attr("x", width - 100)
+					.attr("y", height + margin.bottom)
+					.attr("dy", "-.5em")
+					.style("text-anchor", "middle")
+					.text(xVariable);
+
+				svg.select(".yLabel")
+					.attr("transform", "rotate(-90)")
+					.attr("y", 0 - margin.left)
+					.attr("x", 0 - (height / 2))
+					.attr("dy", "1em")
+					.style("text-anchor", "middle")
+					.text(yVariable);
+			};
+
+			d3.select("#visualizeBtn").on("click", visualizeIt);
+		}
+	};
+});
+
+
+
+
+directives.directive('ngBuildSystems', function() {
+
+	return {
+		restrict: 'A',
+		replace: false,
+		scope: {
+			components: '=',
+			systemDesigns: '='
+		},
+		link: function(scope, element, attrs) {
+
+			// console.log(scope.components);
+
+			function buildSystemDesigns(components) {
+
+				var componentsArray = [];
+				var televisionsArray = [];
+				var speakersArray = [];
+				var amplifersArray = [];
+
+				components.forEach(function(component) {
+
+					componentsArray.push(component.component);
+					if (component.component == 'television') { televisionsArray.push(
+							{
+								'brand': component.brand,
+								'cost': component.cost,
+								'performance': component.performance,
+								'reliability': component.reliability
+							}
+						);
+					};
+					if (component.component == 'speaker') { speakersArray.push(
+							{
+								'brand': component.brand,
+								'cost': component.cost,
+								'performance': component.performance,
+								'reliability': component.reliability
+							}
+						);
+					};
+					if (component.component == 'amplifier') { amplifersArray.push(
+							{
+								'brand': component.brand,
+								'cost': component.cost,
+								'performance': component.performance,
+								'reliability': component.reliability
+							}
+						);
+					};									
+				});
+
+				buildSystems(televisionsArray, amplifersArray, speakersArray);
+			};
+
+			function buildSystems(televisions, amplifiers, speakers) {
+				var sum = 0;
+				var performance = 0;
+				var reliability = 0;
+				var counter = 1;
+
+				for (var i = televisions.length - 1; i >= 0; i--) { // Loop through television container array
+					for (var j = amplifiers.length - 1; j >= 0; j--) { // Loop through amplifier container array
+						for (var k = speakers.length - 1; k >= 0; k--) { // Loop through speaker container array
+							sum = televisions[i].cost + amplifiers[j].cost + speakers[k].cost;
+							performance = (televisions[i].performance + amplifiers[j].performance + speakers[k].performance) / 3;
+							reliability = (televisions[i].reliability + amplifiers[j].reliability + speakers[k].reliability) / 3;
+							scope.systemDesigns.push( // Add system design object to system design container array
+								{
+									"name" : "design " + counter,
+									"cost" : sum,
+									"performance" : performance,
+									"reliability" : reliability,
+									"television" : televisions[i].brand,
+									"amplifier" : amplifiers[j].brand,
+									"speaker" : speakers[k].brand
+								}
+							);
+
+							counter++;
+						};
+					};
 				};
 
-				now();
+				scope.systemDesigns.forEach(function(x) {
+					$("#designsTable").append("<tr><td>" + 
+						x.name + "</td><td>" + 
+						x.television + "</td><td>" + 
+						x.speaker + "</td><td>" + 
+						x.amplifier + "</td><td>"+
+						x.cost + "</td><td>" +
+						x.performance.toFixed(2) + "</td><td>" +
+						x.reliability.toFixed(2) +	"</td></tr>");
+				});
 			};
-			renderGraph();
+
+			scope.$watch('components', function(newVal, oldVal) {
+				if (newVal != undefined && newVal != '[]') {
+					buildSystemDesigns(newVal);
+				}
+			});	
 		}
 	};
 });
